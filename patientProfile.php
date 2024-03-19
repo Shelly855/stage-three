d4a489aa17759593ee56563d9489b499532941c8
sumayyah19 <sumayyahsaleem123@gmail.com>
2024-03-19 04:53:13 +0000

    profile page added

--- patientProfile.php
+++ patientProfile.php
@@ -1,13 +1,30 @@
-<?php require("header.php");?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>Homepage</title>
+</head>
+<body>
+        <div class="container">
+        <?php
+             include("includes/header.php");
+        ?>
         <main>
-        <h2>Profile</h2><br>
+        <h1>Profile</h1><br>
      
 
             
 
         </main>
+        <?php
+        include("includes/footer.php");
+    ?>
+    </div>
+</body>
+</html>
    
 
 
-<?php require("footer.php");?>
