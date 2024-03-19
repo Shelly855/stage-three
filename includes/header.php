db5987e4eb5f1c6386338279bf74aebd7c605d84
sumayyah19 <sumayyahsaleem123@gmail.com>
2024-03-19 04:54:14 +0000

    added href for patientprofile

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
             <li><a href="homepage.php">Home</a></li>
             <li><a href="patientProfile.php">Profiles</a></li>
             <li><a href="appointments.php">Appointments</a></li>
+            <li><a href="notifications.php">Notifications</a></li>
             <li><a href="logout.php">Log Out</a></li>
         </ul>
     </nav>
