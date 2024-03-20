b7f14cc8b135bc47084a6bc7e7da690acef85db6
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-03-20 12:29:28 +0000

    amended header

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,14 @@
 <header>
     <nav>
         <ul>
-            <li><a href="homepage.php">Home</a></li>
-            <li><a href="patientProfile.php">Profiles</a></li>
-            <li><a href="appointments.php">Appointments</a></li>
-            <li><a href="notifications.php">Notifications</a></li>
-            <li><a href="logout.php">Log Out</a></li>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="../patientProfile.php">Profiles</a></li>
+            <li><a href="../appointments/appointments.php">Appointments</a></li>
+            <li><a href="../notifications.php">Notifications</a></li>
+            <li><a href="../logout.php">Log Out</a></li>
         </ul>
     </nav>
 </header>
+<!-- when you put your code files into folders, these links should work. -->
+<!-- if you're putting code into folders, add ../ before css links, footer & header links and links to other pages. -->
+<!-- if not sure, ask me(Shelly) -->
