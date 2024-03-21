24f6c84099c4a17578ac5d148efd3998926a54e3
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-03-21 17:36:31 +0000

    changed Profile to Patients

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
     <nav>
         <ul>
             <li><a href="../index.php">Home</a></li>
-            <li><a href="../patientProfile.php">Profiles</a></li>
+            <li><a href="../patientProfile.php">Patients</a></li>
             <li><a href="../appointments/appointments.php">Appointments</a></li>
             <li><a href="../notifications.php">Notifications</a></li>
             <li><a href="../logout.php">Log Out</a></li>
