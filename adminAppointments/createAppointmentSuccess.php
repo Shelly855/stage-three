46439ecd1d57c56a4103b5d89a4bf70079d71553
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-03-26 18:44:31 +0000

    change

--- adminAppointments/createAppointmentSuccess.php
+++ adminAppointments/createAppointmentSuccess.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
     <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
-    <title><?php echo $message; ?></title>
+    <title><?php echo $title; ?></title>
 </head>
 <body>
     <div class="container">
