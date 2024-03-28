ed5d1701722738b8624d96998913de945cd50231
sumayyah19 <sumayyahsaleem123@gmail.com>
2024-03-28 17:30:17 +0000

    added text

---
+++ patientProfile/helpGuide.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>Help & Support</title>
+</head>
+<body>
+    <div class="container"> 
+        <?php
+            include("../includes/patientHeader.php");
+        ?>  
+        <main>        
+        <h1>Help & Support</h1><br>
+
+
+            
+
+        </main>
+        <?php
+            include("../includes/footer.php");
+        ?>
+    </div>
+</body>
+</html>
