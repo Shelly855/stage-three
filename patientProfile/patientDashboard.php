fa139eeb126025599ddb7d0864922ea2846ccde8
sumayyah19 <sumayyahsaleem123@gmail.com>
2024-03-28 15:41:36 +0000

    editedpatients

---
+++ patientProfile/patientDashboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>Homepage</title>
+</head>
+<body>
+        <div class="container">
+        <?php
+             include("includes/patientHeader.php");
+        ?>
+        <main>
+        <h1>Welcome Sabiha</h1><br>
+     
+
+            
+
+        </main>
+        <?php
+        include("includes/footer.php");
+    ?>
+    </div>
+</body>
+</html>
+   
+
+
