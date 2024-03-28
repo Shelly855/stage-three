c2c2f090a5eb8eed7d441f0bb0cd900dc6198518
sumayyah19 <sumayyahsaleem123@gmail.com>
2024-03-28 15:41:56 +0000

    editedpatients

--- patientProfile/patientProfile.php
+++ patientProfile/patientProfile.php
@@ -3,25 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
-    <link href="css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
-    <title>Homepage</title>
+    <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>My Profile</title>
 </head>
 <body>
-        <div class="container">
+    <div class="container"> 
         <?php
-             include("../includes/patientHeader.php");
-        ?>
+            include("../includes/patientHeader.php");
+        ?>  
         <main>
-        <h1>My Profile</h1><br>
-     
 
             
 
         </main>
         <?php
-        include("includes/footer.php");
-    ?>
+            include("../includes/footer.php");
+        ?>
     </div>
 </body>
 </html>
