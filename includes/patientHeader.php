6da17cd52ae2739e28dd5cfb9a5895535db8ff6b
sumayyah19 <sumayyahsaleem123@gmail.com>
2024-03-28 15:15:19 +0000

    created patient header

---
+++ includes/patientHeader.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="patientProfile.php">Profile</a></li>
+            <li><a href="adminAppointments/appointments.php">Appointments</a></li>
+            <li><a href="notifications.php">Notifications</a></li>
+            <li><a href="logout.php">Log Out</a></li>
+        </ul>
+    </nav>
+</header>
