b0a50810f3c0bc4a2c2c236779fe1dd8f32abebc
sumayyah19 <sumayyahsaleem123@gmail.com>
2024-03-28 15:41:51 +0000

    editedpatients

--- includes/patientHeader.php
+++ includes/patientHeader.php
@@ -1,8 +1,8 @@
 <header>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="patientProfile.php">Profile</a></li>
+            <li><a href="patientProfile/patientDahboard.php">Home</a></li>
+            <li><a href="patientProfile/patientProfile.php">Profile</a></li>
             <li><a href="adminAppointments/appointments.php">Appointments</a></li>
             <li><a href="notifications.php">Notifications</a></li>
             <li><a href="logout.php">Log Out</a></li>
