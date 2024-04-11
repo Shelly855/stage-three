977826580c1b92a305d1682f3c9eb1a3e4b6fb62
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-11 19:58:47 +0100

    Update deletePatient.php

--- adminPatientProfiles/deletePatient.php
+++ adminPatientProfiles/deletePatient.php
@@ -7,11 +7,30 @@ if (isset($_POST['delete'])) {
     if (isset($_POST['pid'])) {
         $patientId = $_POST['pid'];
 
-        $stmt = $db->prepare("DELETE FROM patients WHERE patient_id = :pid");
-        $stmt->bindValue(':pid', $patientId);
-        $result = $stmt->execute();
+        // Get user_id associated with the patient
+        $stmt_user_id = $db->prepare('SELECT user_id FROM patients WHERE patient_id = :pid');
+        $stmt_user_id->bindValue(':pid', $patientId);
+        $result_user_id = $stmt_user_id->execute();
+        $row = $result_user_id->fetchArray(SQLITE3_ASSOC);
+        $userId = $row['user_id'];
 
-        if ($result) {
+        // Get first name and surname of the patient from the users table
+        $stmt_user_info = $db->prepare('SELECT first_name, surname FROM users WHERE user_id = :uid');
+        $stmt_user_info->bindValue(':uid', $userId);
+        $result_user_info = $stmt_user_info->execute();
+        $user_info = $result_user_info->fetchArray(SQLITE3_ASSOC);
+        $firstName = $user_info['first_name'];
+        $surname = $user_info['surname'];
+
+        $stmt_patient_delete = $db->prepare("DELETE FROM patients WHERE patient_id = :pid");
+        $stmt_patient_delete->bindValue(':pid', $patientId);
+        $result_patient_delete = $stmt_patient_delete->execute();
+
+        $stmt_user_delete = $db->prepare("DELETE FROM users WHERE user_id = :uid");
+        $stmt_user_delete->bindValue(':uid', $userId);
+        $result_user_delete = $stmt_user_delete->execute();
+
+        if ($result_patient_delete && $result_user_delete) {
             header("Location: ../adminPatientProfiles/deletePatientSuccess.php?deleted=true");
             exit();
         } else {
@@ -21,11 +40,12 @@ if (isset($_POST['delete'])) {
 }
 
 if (isset($_GET['pid'])) {
-    $stmt = $db->prepare('SELECT * FROM patients WHERE patient_id = :pid');
+    $stmt = $db->prepare('SELECT patients.*, users.first_name, users.surname FROM patients INNER JOIN users ON patients.user_id = users.user_id WHERE patients.patient_id = :pid');
     $stmt->bindParam(':pid', $_GET['pid'], SQLITE3_INTEGER);
     $result = $stmt->execute();
     $patient = $result->fetchArray(SQLITE3_ASSOC);
 }
+
 
 $db->close();
 ?>
@@ -45,13 +65,17 @@ $db->close();
             <h2>Delete Patient</h2>
             <div class="confirm">Are you sure you want to delete this patient?</div>
             <?php if(isset($patient)): ?>
-            <div class="delete-data">
+                <div class="delete-data">
                 <label class="delete-label">First Name:</label>
                 <label><?php echo $patient['first_name']; ?></label>
             </div>
             <div class="delete-data">
                 <label class="delete-label">Surname:</label>
                 <label><?php echo $patient['surname']; ?></label>
+            </div>
+            <div class="delete-data">
+                <label class="delete-label">Email:</label>
+                <label><?php echo $patient['email']; ?></label>
             </div>
             <div class="delete-data">
                 <label class="delete-label">Date of Birth:</label>
