01b046517b11f81dc0150142652e20c1e336dd7d
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-11 20:34:09 +0100

    Create patientHeader.php

---
+++ includes/patientHeader.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="../patientDashboard.php">Home</a></li>
+            <li><a href="../logout.php">Log Out</a></li>
+        </ul>
+    </nav>
+</header>
