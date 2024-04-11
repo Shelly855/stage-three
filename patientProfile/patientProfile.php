b8f219ff6dd5a9970efbe27b9b861e8d10f9c788
sumayyah19 <149150265+sumayyah19@users.noreply.github.com>
2024-04-11 16:39:21 +0100

    Create patientProfile.php

---
+++ patientProfile/patientProfile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>My Profile</title>
+</head>
+<body>
+    <div class="container"> 
+        <?php
+            include("../includes/patientHeader.php");
+        ?>  
+        <main>
+            <h1>My Profile<h1> 
+                <br>
+        <div class="row patientMenu">
+            <div class="col-3">
+                <a href="viewPatientDetails.php">My Details</a> 
+            </div>
+            <div class="col-3">
+                <a href="viewMedicalHistory.php">My Medical History</a>
+            </div> 
+            <div class="col-3">
+                <a href="helpGuide.php">Help & Support</a>
+            </div>
+        </main>
+        <?php
+            include("../includes/footer.php");
+        ?>
+    </div>
+</body>
+</html>
