f812a37a9a9721b104fcfcbd1e65985ea1091c0a
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-11 20:38:01 +0100

    Update patientHeader.php

--- includes/patientHeader.php
+++ includes/patientHeader.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul>
-            <li><a href="../patientDashboard.php">Home</a></li>
+            <li><a href="../patientProfile/dashboardPatient.php">Home</a></li>
             <li><a href="../logout.php">Log Out</a></li>
         </ul>
     </nav>
