33558705d77eece899525166e9a276cfcea26c16
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-04-11 14:10:53 +0100

    indentation

--- login/index.php
+++ login/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,26 +7,19 @@
     <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
     <title>Homepage</title>
 </head>
-
 <body>
-    <div class="container">
-
-        <main>
-            <h1>Welcome!</h1>
-            <nav>
-                <a href="../login/patientLogin.php" class="login">Patient Login</a>
-                <a href="../login/doctorLogin.php" class="login">Doctor Login</a>
-                <a href="../login/adminLogin.php" class="login">Admin Login</a>
-            </nav>
-        </main>
-
-    </div>
-</body>
-
-</html>
-    <?php
-        include("../includes/footer.php");
-    ?>
+        <div class="container">
+            <main>
+                <h1>Welcome!</h1>
+                <nav>
+                    <a href="../login/patientLogin.php" class="login">Patient Login</a>
+                    <a href="../login/doctorLogin.php" class="login">Doctor Login</a>
+                    <a href="../login/adminLogin.php" class="login">Admin Login</a>
+                </nav>
+            </main>
+        <?php
+            include("../includes/footer.php");
+        ?>
     </div>
 </body>
 </html>
