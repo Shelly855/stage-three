9a7ea815f011e70fae4a754d1c1917d1603bb61e
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-11 17:27:13 +0100

    Create createUser.php

---
+++ adminUsers/createUser.php
@@ -0,0 +1,101 @@
+<?php
+
+$errorfname = $errorsurname = $errorrole = $erroruname = $errorpwd = "";
+$allFields = true;
+
+if (isset($_POST['submit'])) {
+    $db = new SQLITE3('C:\xampp\data\stage_3.db');
+
+    if (empty($_POST['fname'])) {
+        $errorfname = "First Name is mandatory";
+        $allFields = false;
+    }
+    if (empty($_POST['surname'])) {
+        $errorsurname = "Surname is mandatory";
+        $allFields = false;
+    }
+    if (empty($_POST['role'])) {
+        $errorrole = "Role is mandatory";
+        $allFields = false;
+    }
+    if (empty($_POST['uname'])) {
+        $erroruname = "Username is mandatory";
+        $allFields = false;
+    }
+    if (empty($_POST['pwd'])) {
+        $errorpwd = "Password is mandatory";
+        $allFields = false;
+    }
+
+    if ($allFields) {
+        $stmt = $db->prepare('INSERT INTO users (first_name, surname, role, username, password) VALUES (:fname, :surname, :role, :uname, :pwd)');
+        $stmt->bindValue(':fname', $_POST['fname'], SQLITE3_TEXT);
+        $stmt->bindValue(':surname', $_POST['surname'], SQLITE3_TEXT);
+        $stmt->bindValue(':role', $_POST['role'], SQLITE3_TEXT);
+        $stmt->bindValue(':uname', $_POST['uname'], SQLITE3_TEXT);
+        $stmt->bindValue(':pwd', $_POST['pwd'], SQLITE3_TEXT);
+            
+        $result = $stmt->execute();
+    
+        if ($result) {
+            header("Location: ../adminUsers/createUserSuccess.php?createUser=success");
+            exit();
+        } else {
+            echo "Error creating appointment";
+        }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>Create User</title>
+</head>
+<body>
+    <div class="container"> 
+        <?php
+            include("../includes/adminHeader.php");
+        ?>  
+        <main>
+            <h1>Create User</h1>
+            <form method="post">
+                <label>First Name</label>
+                <input type="text" name="fname" value="<?php echo isset($_POST['fname']) ? $_POST['fname'] : ''; ?>">
+                <span class="blank-error"><?php echo $errorfname; ?></span>
+
+                <label>Surname</label>
+                <input type="text" name="surname" value="<?php echo isset($_POST['surname']) ? $_POST['surname'] : ''; ?>">
+                <span class="blank-error"><?php echo $errorsurname; ?></span>
+
+                <label>Role</label>
+                <select name="role">
+                    <option value="">Select Role</option>
+                    <option value="admin">Admin</option>
+                    <option value="doctor">Doctor</option>
+                    <option value="patient">Patient</option>
+                </select>
+                <span class="blank-error"><?php echo $errorrole; ?></span>
+
+                <label>Username</label>
+                <input type="text" name="uname" value="<?php echo isset($_POST['uname']) ? $_POST['uname'] : ''; ?>">
+                <span class="blank-error"><?php echo $erroruname; ?></span>
+
+                <label>Password</label>
+                <input type="text" name="pwd" value="<?php echo isset($_POST['pwd']) ? $_POST['pwd'] : ''; ?>">
+                <span class="blank-error"><?php echo $errorpwd; ?></span>
+
+                <input type="submit" value="Create User" name="submit">
+                <a href="../adminUsers/adminUsers.php" class="back-button">Back</a>
+            </form>
+        </main>
+        <?php
+            include("../includes/footer.php");
+        ?>
+    </div>
+</body>
+</html>
