cd5830006228b038aac82c5494462e6175f43876
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-11 22:28:05 +0100

    Create viewSurgerySql.php

---
+++ proposedSurgery/viewSurgerySql.php
@@ -0,0 +1,32 @@
+<?php
+
+function getSurgeries() {
+    $db = new SQLite3('C:\xampp\data\stage_3.db');
+
+    if (!$db) {
+        die("Failed to connect to the database.");
+    }
+
+    $sql = "SELECT s.surgery_id, s.surgery_name, u.first_name, u.surname, s.eligible 
+            FROM surgery s
+            JOIN patients p ON s.patient_id = p.patient_id
+            JOIN users u ON p.user_id = u.user_id";
+
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute();
+
+    if (!$result) {
+        die("Error executing query: " . $db->lastErrorMsg());
+    }
+
+    $arrayResult = [];
+    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        $arrayResult[] = $row;
+    }
+
+    $db->close();
+
+    return $arrayResult;
+}
+
+?>
