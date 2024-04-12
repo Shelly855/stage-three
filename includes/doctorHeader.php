5044962ff38598d19c5e441b19a7efd81e01022d
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-12 10:59:46 +0100

    Update doctorHeader.php

--- includes/doctorHeader.php
+++ includes/doctorHeader.php
@@ -2,7 +2,8 @@
     <nav>
         <ul>
             <li><a href="../doctorProfile/dashboardDoctor.php">Home</a></li>
-            <li><a href="../doctorAppointments/adminAppointments.php">Appointments</a></li>
+            <li><a href="../proposedSurgery/proposedSurgery.php">Surgery</a></li>
+            <li><a href="../doctorAppointments/doctorAppointments.php">Appointments</a></li>
             <li><a href="../notifications.php">Notifications</a></li>
             <li><a href="../login/logout.php">Log Out</a></li>
         </ul>
