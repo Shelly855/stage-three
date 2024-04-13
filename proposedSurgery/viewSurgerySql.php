0308c935fce5025b7cad6f081dd35c0a73afcd78
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-13 19:51:47 +0100

    Update viewSurgerySql.php

--- proposedSurgery/viewSurgerySql.php
+++ proposedSurgery/viewSurgerySql.php
@@ -7,10 +7,11 @@ function getSurgeries() {
         die("Failed to connect to the database.");
     }
 
-    $sql = "SELECT s.surgery_id, s.surgery_name, u.first_name, u.surname, s.eligible 
-            FROM surgery s
-            JOIN patients p ON s.patient_id = p.patient_id
-            JOIN users u ON p.user_id = u.user_id";
+    $sql = "SELECT s.surgery_id, s.surgery_name, u.first_name, u.surname, s.eligible, poa.completed 
+    FROM surgery s
+    JOIN patients p ON s.patient_id = p.patient_id
+    JOIN users u ON p.user_id = u.user_id
+    LEFT JOIN POA_questionnaire poa ON s.surgery_id = poa.surgery_id";
 
     $stmt = $db->prepare($sql);
     $result = $stmt->execute();
