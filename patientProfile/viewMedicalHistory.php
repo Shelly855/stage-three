73f7e96f7115e608b739b65e62348bc3a1d3dc41
sumayyah19 <149150265+sumayyah19@users.noreply.github.com>
2024-04-13 23:53:55 +0100

    Update viewMedicalHistory.php

--- patientProfile/viewMedicalHistory.php
+++ patientProfile/viewMedicalHistory.php
@@ -18,7 +18,7 @@ $row = $res->fetchArray(SQLITE3_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
     <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
-    <title>My Details</title>
+    <title>My Medical History</title>
 </head>
 <body>
     <div class="container"> 
