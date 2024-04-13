68ad5eb1d303dc8b88239b0d9845e9a91dcefd74
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-13 12:04:18 +0100

    changed location of logout link

--- includes/patientHeader.php
+++ includes/patientHeader.php
@@ -1,11 +1,11 @@
 <header>
     <nav>
         <ul>
-            <li><a href="../patientProfile/dashboardPatient.php">Home</a></li>
-            <li><a href="../login/logout.php">Log Out</a></li>      
+            <li><a href="../patientProfile/dashboardPatient.php">Home</a></li>   
             <li><a href="patientProfile.php">Profile</a></li>
             <li><a href="patientAppointments.php">Appointments</a></li>
             <li><a href="patientNotifications.php">Notifications</a></li>
+             <li><a href="../login/logout.php">Log Out</a></li>   
         </ul>
     </nav>
 </header>
