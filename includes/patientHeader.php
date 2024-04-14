da5bd528d75a5cc6a4157493c624c5b6ac32bfbe
sumayyah19 <149150265+sumayyah19@users.noreply.github.com>
2024-04-14 22:33:55 +0100

    Update patientHeader.php

--- includes/patientHeader.php
+++ includes/patientHeader.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href="../patientProfile/dashboardPatient.php">Home</a></li>   
             <li><a href="patientProfile.php">Profile</a></li>
-            <li><a href="patientAppointment.php">Appointments</a></li>
+            <li><a href="upcomingAppointments.php">Appointments</a></li>
             <li><a href="patientNotifications.php">Notifications</a></li>
              <li><a href="../login/logout.php">Log Out</a></li>   
         </ul>
