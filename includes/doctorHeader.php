af9fb2179d217a8e3eef4b64a0036621310f6de2
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-15 11:40:44 +0100

    Update doctorHeader.php

--- includes/doctorHeader.php
+++ includes/doctorHeader.php
@@ -6,7 +6,6 @@
             <li><a href="../doctorProfile/viewPOAanswers.php">Pre-Operative Answers</a></li>
             <li><a href="../proposedSurgery/proposedSurgery.php">Surgery</a></li>
             <li><a href="../doctorAppointments/doctorAppointments.php">Appointments</a></li>
-            <li><a href="../notifications.php">Notifications</a></li>
             <li><a href="../login/logout.php">Log Out</a></li>
         </ul>
     </nav>
