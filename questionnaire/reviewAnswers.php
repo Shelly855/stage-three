a4a144c179ac39655f323993dc210de722092af2
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-15 13:05:37 +0100

    Update reviewAnswers.php

--- questionnaire/reviewAnswers.php
+++ questionnaire/reviewAnswers.php
@@ -5,12 +5,75 @@ $db = new SQLite3('C:\xampp\data\stage_3.db');
 $patientId = $_SESSION['patient_id'];
 $surgeryId = $_SESSION['surgery_id'];
 
+function checkFieldsBlank($db, $surgeryId) {
+    $stmt = $db->prepare('SELECT * FROM POA_questionnaire WHERE surgery_id = :surgery_id');
+    $stmt->bindValue(':surgery_id', $surgeryId, SQLITE3_INTEGER);
+    $result = $stmt->execute();
+
+    if (!$result) {
+        die("Error executing query: " . $db->lastErrorMsg());
+    }
+
+    $poaData = $result->fetchArray(SQLITE3_ASSOC);
+
+    if (
+        $poaData['date_of_poa'] !== null && $poaData['date_of_poa'] !== '' &&
+        $poaData['surname'] !== null && $poaData['surname'] !== '' &&
+        $poaData['first_name'] !== null && $poaData['first_name'] !== '' &&
+        $poaData['address'] !== null && $poaData['address'] !== '' &&
+        $poaData['date_of_birth'] !== null && $poaData['date_of_birth'] !== '' &&
+        $poaData['sex'] !== null && $poaData['sex'] !== '' &&
+        $poaData['age'] !== null && $poaData['age'] !== '' &&
+        $poaData['telephone_number'] !== null && $poaData['telephone_number'] !== '' &&
+        $poaData['occupation'] !== null && $poaData['occupation'] !== '' &&
+        $poaData['religion'] !== null && $poaData['religion'] !== '' &&
+        $poaData['emergency_contact_number'] !== null && $poaData['emergency_contact_number'] !== '' &&
+        $poaData['heart_disease'] !== null && $poaData['heart_disease'] !== '' &&
+        $poaData['MI'] !== null && $poaData['MI'] !== '' &&
+        $poaData['hypertension'] !== null && $poaData['hypertension'] !== '' &&
+        $poaData['angina'] !== null && $poaData['angina'] !== '' &&
+        $poaData['DVT/PE'] !== null && $poaData['DVT/PE'] !== '' &&
+        $poaData['stroke'] !== null && $poaData['stroke'] !== '' &&
+        $poaData['diabetes'] !== null && $poaData['diabetes'] !== '' &&
+        $poaData['epilepsy'] !== null && $poaData['epilepsy'] !== '' &&
+        $poaData['jaundice'] !== null && $poaData['jaundice'] !== '' &&
+        $poaData['sickle_cell_status'] !== null && $poaData['sickle_cell_status'] !== '' &&
+        $poaData['kidney_disease'] !== null && $poaData['kidney_disease'] !== '' &&
+        $poaData['arthritis'] !== null && $poaData['arthritis'] !== '' &&
+        $poaData['asthma'] !== null && $poaData['asthma'] !== '' &&
+        $poaData['pregnant'] !== null && $poaData['pregnant'] !== '' &&
+        $poaData['other_health_conditions'] !== null && $poaData['other_health_conditions'] !== '' &&
+        $poaData['previous_medication'] !== null && $poaData['previous_medication'] !== ''
+    ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+if (isset($_POST['submit'])) {
+    if (!checkFieldsBlank($db, $surgeryId)) {
+        $query = "UPDATE POA_questionnaire SET completed = 1, percentage_completed = 100 WHERE surgery_id = :surgery_id";
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(':surgery_id', $surgeryId, SQLITE3_INTEGER);
+        $stmt->execute();
+
+        header("Location: ../questionnaire/submitQuestionnaireSuccess.php");
+        exit();
+    } else {
+        header("Location: ../questionnaire/reviewAnswers.php?error=1");
+        exit();
+    }
+}
+
 $query = "SELECT * FROM POA_questionnaire WHERE surgery_id = :surgery_id";
 $stmt = $db->prepare($query);
 $stmt->bindValue(':surgery_id', $surgeryId, SQLITE3_INTEGER);
 $result = $stmt->execute();
 
 $row = $result->fetchArray();
+
 ?>
 
 <!DOCTYPE html>
@@ -67,10 +130,10 @@ $row = $result->fetchArray();
                 <h2>Additional Details</h2>
                 <p>Are you currently or is there a possibility you are pregnant?: <?php echo $row['pregnant'] ? 'Yes' : 'No'; ?></p>
                 <p>Other Health Conditions: <?php echo $row['other_health_conditions']; ?></p>
-                <p>Previous Medication <?php echo $row['previous_medication']; ?></p>
+                <p>Previous Medication: <?php echo $row['previous_medication']; ?></p>
             </div>
-            <form action="../questionnaire/submitAnswers.php" method="post">
-                <input type="submit" value="Submit Answers">
+            <form action="../questionnaire/reviewAnswers.php" method="post">
+                <input type="submit" name="submit" value="Submit Answers">
             </form>
             <form id="questionnaire-back-button">
             <a href="../questionnaire/questionnaire.php">Back to Questionnaire</a> 
