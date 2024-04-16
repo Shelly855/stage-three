5b3af897291188bbee6dc7be8bd8389d9e417051
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-16 14:06:57 +0100

    db connection

--- adminUsers/updateUser.php
+++ adminUsers/updateUser.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$db = new SQLITE3('C:\xampp\data\stage_3.db');
+include '../includes/dbConnection.php';
 $errorfname = $errorsurname = $errorrole = $erroruname = $errorpwd = "";
 $allFields = true;
 
