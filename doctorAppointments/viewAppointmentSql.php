3f7772ca223e02cb11aca9b62c5de6f7809bc690
sabihadin <147169336+sabihadin@users.noreply.github.com>
2024-04-16 21:10:13 +0100

    Update viewAppointmentSql.php

--- doctorAppointments/viewAppointmentSql.php
+++ doctorAppointments/viewAppointmentSql.php
@@ -1,5 +1,6 @@
 <?php
-function getAppointments($doctor_id) {
+
+function getAppointments() {
     $db = new SQLITE3('C:\xampp\data\stage_3.db');
 
     if (!$db) {
@@ -11,27 +12,23 @@ function getAppointments($doctor_id) {
         appointments.appointment_id,
         appointments.date,
         appointments.time,
-        appointments.clinical_notes,
-        patients.medical_conditions,
-        patients.previous_medical_conditions,
-        patient.first_name AS patient_first_name,
-        patient.surname AS patient_surname,
-        staff.first_name AS staff_first_name,
-        staff.surname AS staff_surname
+        users.first_name AS patient_first_name,
+        users.surname AS patient_surname,
+        users.first_name AS staff_first_name,
+        users.surname AS staff_surname
     FROM 
         appointments
+
     JOIN 
         patients ON appointments.patient_id = patients.patient_id
     JOIN 
-        users AS patient ON appointments.patient_id = patient.user_id
-    JOIN 
-        users AS staff ON appointments.user_id = staff.user_id
+        users ON appointments.user_id = users.user_id;
+        
     WHERE 
-        appointments.user_id = :doctor_id";
+        appointments.users_id = :doctor_id";
 
     $stmt = $db->prepare($sql);
-    $stmt->bindParam(':doctor_id', $doctor_id, SQLITE3_INTEGER);
-
+    
     $result = $stmt->execute();
 
     if (!$result) {
