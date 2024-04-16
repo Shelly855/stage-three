d3ad9a4c54b8da04b04ce17a2851df1b15080729
sabihadin <147169336+sabihadin@users.noreply.github.com>
2024-04-16 17:21:54 +0100

    Update viewAppointmentSql.php

--- adminAppointments/viewAppointmentSql.php
+++ adminAppointments/viewAppointmentSql.php
@@ -1,6 +1,6 @@
 <?php
 
-function getAppointments (){
+function getAppointments() {
     $db = new SQLITE3('C:\xampp\data\stage_3.db');
 
     if (!$db) {
@@ -12,17 +12,23 @@ function getAppointments (){
         appointments.appointment_id,
         appointments.date,
         appointments.time,
-        patients.first_name AS patient_first_name,
-        patients.surname AS patient_surname,
-        staff.first_name AS staff_first_name,
-        staff.surname AS staff_surname
+        users.first_name AS patient_first_name,
+        users.surname AS patient_surname,
+        users.first_name AS staff_first_name,
+        users.surname AS staff_surname
     FROM 
         appointments
+
     JOIN 
         patients ON appointments.patient_id = patients.patient_id
     JOIN 
-        staff ON appointments.staff_id = staff.staff_id";
+        users ON appointments.user_id = users.user_id;
+        
+    WHERE 
+        appointments.users_id = :doctor_id";
+
     $stmt = $db->prepare($sql);
+    
     $result = $stmt->execute();
 
     if (!$result) {
