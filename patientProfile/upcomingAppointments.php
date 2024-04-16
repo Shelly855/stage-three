a562b976f72713598b5f30ea178d1c517c9561da
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-16 14:30:43 +0100

    db connection

--- patientProfile/upcomingAppointments.php
+++ patientProfile/upcomingAppointments.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$db = new SQLite3('C:\xampp\data\stage_3.db');
+include '../includes/dbConnection.php';
 
 if (!$db) {
     die("Failed to connect to the database.");
@@ -9,7 +9,6 @@ if (!$db) {
 $patient = $_SESSION['patient_id'];
 $query = "SELECT * FROM appointments WHERE patient_id='$patient'";
 $res = $db->query($query);
-
 ?>
 
 <!DOCTYPE html>
@@ -23,44 +22,43 @@ $res = $db->query($query);
 </head>
 <body>
     <div class="container"> 
-        <?php include("../includes/patientHeader.php"); ?>  
-        <main> 
-            <?php
-            if ($res) {
-                $row = $res->fetchArray(SQLITE3_ASSOC);
-
-                if ($row !== false) {
-            ?>
-            <h1>Appointments</h1>
-            <table class="detailsTable">
-                <tr> 
-                    <td>Patient ID</td>
-                    <td><?php echo $row['patient_id']; ?></td>
-                </tr>
-                <tr> 
-                    <td>Appointment ID</td>
-                    <td><?php echo $row['appointment_id']; ?></td>
-                </tr>
-                <tr> 
-                    <td>Appointment Date</td>
-                    <td><?php echo $row['date']; ?></td>
-                </tr>            
-                <tr> 
-                    <td>Appointment Time</td>
-                    <td><?php echo $row['time']; ?></td>
-                </tr>
-            </table>  
-            <?php
+            <?php include("../includes/patientHeader.php"); ?>  
+            <main> 
+                <?php
+                if ($res) {
+                    $row = $res->fetchArray(SQLITE3_ASSOC);
+    
+                    if ($row !== false) {
+                ?>
+                <h1>Appointments</h1>
+                <table class="detailsTable">
+                    <tr> 
+                        <td>Patient ID</td>
+                        <td><?php echo $row['patient_id']; ?></td>
+                    </tr>
+                    <tr> 
+                        <td>Appointment ID</td>
+                        <td><?php echo $row['appointment_id']; ?></td>
+                    </tr>
+                    <tr> 
+                        <td>Appointment Date</td>
+                        <td><?php echo $row['date']; ?></td>
+                    </tr>            
+                    <tr> 
+                        <td>Appointment Time</td>
+                        <td><?php echo $row['time']; ?></td>
+                    </tr>
+                </table>  
+                <?php
+                    } else {
+                        echo "<h1>No Upcoming Appointments</h1>";
+                    }
                 } else {
-                    echo "<h1>No Upcoming Appointments</h1>";
+                    echo "<h1>Error executing query.</h1>";
                 }
-            } else {
-                echo "<h1>Error executing query.</h1>";
-            }
-            ?>
-        </main>
-    <?php include("../includes/footer.php"); ?>    
+                ?>
+            </main>
+        <?php include("../includes/footer.php"); ?>    
     </div>
-
 </body>
 </html>
