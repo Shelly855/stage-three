21df422e0c6f261bf2bb005289056762d67d858a
sumayyah19 <149150265+sumayyah19@users.noreply.github.com>
2024-04-18 00:30:58 +0100

    Create requestSubmitted.php

---
+++ patientProfile/requestSubmitted.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>Help & Support</title>
+</head>
+<body>
+    <div class="container"> 
+        <?php
+            include("../includes/patientHeader.php");
+        ?>  
+        <main>
+            <h1>Help Guide<h1> 
+                <h2>Your request has been submitted. We will get back to you shortly.</h2>
+                <p class="backPatient"><a href="helpGuide.php">Back</a></p>      
+        </main>
+        <?php
+            include("../includes/footer.php");
+        ?>
+    </div>
+</body>
+</html>
