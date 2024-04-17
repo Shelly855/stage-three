368211209ec1254820ec14dd43a36604151e7207
sumayyah19 <149150265+sumayyah19@users.noreply.github.com>
2024-04-17 21:08:25 +0100

    Create surgeryDetails.php

---
+++ patientProfile/surgeryDetails.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+include '../includes/dbConnection.php';
+
+if (!$db) {
+    die("Failed to connect to the database.");
+}
+
+$patient = $_SESSION['patient_id'];
+$query = "SELECT * FROM surgery WHERE patient_id='$patient'";
+$res = $db->query($query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="../css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <title>Surgery Details</title>
+</head>
+<body>
+    <div class="container"> 
+            <?php include("../includes/patientHeader.php"); ?>  
+            <main> 
+                <?php
+                if ($res) {
+                    $row = $res->fetchArray(SQLITE3_ASSOC);
+    
+                    if ($row !== false) {
+                ?>
+                <h1>Information about your surgery</h1>
+                <table class="detailsTable">
+                    <tr> 
+                        <td>Surgery ID</td>
+                        <td><?php echo $row['surgery_id']; ?></td>
+                    </tr>
+                    <tr> 
+                        <td>Appointment ID</td>
+                        <td><?php echo $row['surgery_name']; ?></td>
+                    </tr>
+                    <tr> 
+                        <td>Appointment Date</td>
+                        <td><?php echo $row['patient_id']; ?></td>
+                    </tr>            
+                    <tr> 
+                        <td>Appointment Time</td>
+                        <td><?php echo $row['eligible']; ?></td>
+                    </tr>
+                </table>  
+                <?php
+                    } 
+                    else {
+                        echo "<h1>You have no surgery booked</h1>";
+                    }
+                } else {
+                    echo "<h1>Error</h1>";
+                }
+                ?>
+            </main>
+        <?php include("../includes/footer.php"); ?>    
+    </div>
+</body>
+</html>
