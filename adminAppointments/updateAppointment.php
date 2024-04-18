d8614ca27f5f6bb9312d1d6193b4d63f8f52c4eb
sabihadin <147169336+sabihadin@users.noreply.github.com>
2024-04-18 15:43:24 +0100

    Update updateAppointment.php

--- adminAppointments/updateAppointment.php
+++ adminAppointments/updateAppointment.php
@@ -10,13 +10,14 @@
 <body>
     <div class="container">
         <?php
-        include("../includes/adminHeader.php");
+        include("../includes/doctorHeader.php");
         
         $db = new SQLITE3('C:\xampp\data\stage_3.db');
         $sql = "SELECT * FROM appointments WHERE appointment_id = :aid";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':aid', $_GET['aid'], SQLITE3_TEXT);
         $result= $stmt->execute();
+        $appointment = $result->fetchArray(SQLITE3_ASSOC);
         $arrayResult = [];
         
         while($row=$result->fetchArray(SQLITE3_NUM)){
@@ -24,8 +25,25 @@
         }
         
 
-        $erroraid = $errordate = $errortime = $errorpid = $errorsid = "";
+        $erroraid = $errordate = $errortime = "";
         $allFields = true;
+
+        
+        $patient_name = getPatientName($db, $appointment['patient_id']);
+
+        function getPatientName($db, $patient_id)
+        {
+            $stmt = $db->prepare("SELECT u.first_name, u.surname FROM patients p JOIN users u ON p.user_id = u.user_id WHERE p.patient_id = :pid");
+            $stmt->bindValue(':pid', $patient_id, SQLITE3_INTEGER);
+            $result = $stmt->execute();
+            $patient = $result->fetchArray(SQLITE3_ASSOC);
+
+            if ($patient) {
+                return $patient['first_name'] . ' ' . $patient['surname'];
+            } else {
+                return 'Unknown';
+            }
+        }
 
         if (isset($_POST['submit'])) {
 
@@ -37,22 +55,13 @@
                 $errortime = "Time is mandatory";
                 $allFields = false;
             }
-            if (empty($_POST['pid'])) {
-                $errorpid = "Patient ID is mandatory";
-                $allFields = false;
-            }
-            if (empty($_POST['sid'])) {
-                $errorsid = "Staff ID is mandatory";
-                $allFields = false;
-            }
 
             if ($allFields) {
 
-                $stmt = $db->prepare("UPDATE appointments SET date = :date, time = :time, patient_id = :pid, staff_id = :sid WHERE appointment_id = :aid");
+                $stmt = $db->prepare("UPDATE appointments SET date = :date, time = :timeWHE RE appointment_id = :aid");
                 $stmt->bindValue(':date', $_POST['date']);
                 $stmt->bindValue(':time', $_POST['time']);
-                $stmt->bindValue(':pid', $_POST['pid']);
-                $stmt->bindValue(':sid', $_POST['sid']);
+              
                 $stmt->bindValue(':aid', $_GET['aid']);
 
                 $result = $stmt->execute();
@@ -64,13 +73,19 @@
                     echo "Error updating appointment.";
                 }
             }
-            
         }
 
         ?>
         <main>
             <h1>Update Appointment</h1>
             <form method="post">
+            <?php if (isset($patient)): ?>
+                    <input type="hidden" name="patient_id" value="<?php echo $patient['patient_id']; ?>">
+                <?php endif; ?>
+                <div id="patient-update">
+                    <label>Patient Name:</label>
+                    <span><?php echo isset($patient_name) ? $patient_name : ''; ?></span>
+                </div>
                 <label>Date</label>
                 <input type="date" name="date" value="<?php echo $arrayResult[0][1]; ?>">
                 <span class="blank-error"><?php echo $errordate; ?></span>
@@ -79,13 +94,7 @@
                 <input type="time" name="time" value="<?php echo $arrayResult[0][2]; ?>">
                 <span class="blank-error"><?php echo $errortime; ?></span>
 
-                <label>Patient ID</label>
-                <input type="number" name="pid" value="<?php echo $arrayResult[0][5]; ?>">
-                <span class="blank-error"><?php echo $errorpid; ?></span>
-
-                <label>Staff ID</label>
-                <input type="number" name="sid" value="<?php echo $arrayResult[0][6]; ?>">
-                <span class="blank-error"><?php echo $errorsid; ?></span>
+            
 
                 <input type="submit" name="submit" value="Update"><a href="../adminAppointments/adminAppointments.php" class="back-button">Back</a>
             </form>
