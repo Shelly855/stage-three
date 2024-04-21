02b1f48d7b8732c823eb51e7680a1f85595bb0a0
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-21 11:53:34 +0100

    Update adminHeader.php

--- includes/adminHeader.php
+++ includes/adminHeader.php
@@ -4,12 +4,9 @@
             <li><a href="../adminProfile/dashboardAdmin.php">Home</a></li>
             <li><a href="../adminPatientProfiles/adminPatientProfiles.php">Patients</a></li>
             <li><a href="../adminUsers/adminUsers.php">Users</a></li>
+            <li><a href="../adminProfile/adminProposedSurgery.php">Surgery</a></li>
             <li><a href="../adminAppointments/adminAppointments.php">Appointments</a></li>
             <li><a href="../login/logout.php">Log Out</a></li>
         </ul>
     </nav>
 </header>
-<!-- when you put your code files into folders, these links should work. -->
-<!-- if you're putting code into folders, add ../ before css links, footer & header links and links to other pages. -->
-<!-- if not sure, ask me(Shelly) -->
-
