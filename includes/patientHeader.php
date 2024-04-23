4accd2d694f06655e36cdd01898cdb81dbc7410a
Shelly855 <149001895+Shelly855@users.noreply.github.com>
2024-04-23 13:47:23 +0100

    Update patientHeader.php

--- includes/patientHeader.php
+++ includes/patientHeader.php
@@ -2,11 +2,10 @@
     <nav>
         <ul>
             <li><a href="../patientProfile/dashboardPatient.php">Home</a></li>   
-            <li><a href="patientProfile.php">Profile</a></li>
-            <li><a href="upcomingAppointments.php">Appointments</a></li>
-            <li><a href="patientNotifications.php">Notifications</a></li>
+            <li><a href="../patientProfile/patientProfile.php">Profile</a></li>
+            <li><a href="../patientProfile/upcomingAppointments.php">Appointments</a></li>
+            <li><a href="../patientProfile/patientNotifications.php">Notifications</a></li>
              <li><a href="../login/logout.php">Log Out</a></li>   
         </ul>
     </nav>
 </header>
-
